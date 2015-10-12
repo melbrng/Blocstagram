08a21b1a11d74a8755b5eb4b0a2d5c9522a1e135
Melissa <melbrng@gmail.com>
2015-10-12 13:36:09 -0600

    finished checkpoint exercise 27

--- Blocstagram/Blocstagram/AppDelegate.m
+++ Blocstagram/Blocstagram/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "Viewcontroller.h"
+#import "ImagesTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -22,7 +22,7 @@
     
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ImagesTableViewController alloc] init]];
     
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Melissa Boring on 10/12/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@end
