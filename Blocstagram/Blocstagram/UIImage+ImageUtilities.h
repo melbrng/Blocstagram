cbc5f9accc130aa2c3869e2e6517b3a521ea4905
Melissa <melbrng@gmail.com>
2015-11-14 12:37:48 -0700

    reset master to checkpoint-41-pictures code

--- Blocstagram/Blocstagram/UIImage+ImageUtilities.h
+++ Blocstagram/Blocstagram/UIImage+ImageUtilities.h
@@ -13,6 +13,5 @@
 - (UIImage *) imageWithFixedOrientation;
 - (UIImage *) imageResizedToMatchAspectRatioOfSize:(CGSize)size;
 - (UIImage *) imageCroppedToRect:(CGRect)cropRect;
-- (UIImage *) imageByScalingToSize:(CGSize)size andCroppingWithRect:(CGRect)rect;
 
 @end
